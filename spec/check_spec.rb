665c68f5b9c88418156a12d0797de3b3cc450b0e
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-02-03 08:48:22 +0900

    Write test

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -5,7 +5,8 @@ describe "Men's points" do
   it "passing" do
     File::open("spec/passing/test.json") do |io|
       data = JSON.load(io)
-      p data
+      data["name"].should eq "reikubonaga"
+      data["answers"].should eq ["3", "2", "1", "3"]
     end
   end
 end
