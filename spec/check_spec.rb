6eafda6e6ee197983be8836d2a8d7844f1484a5b
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-02-03 09:10:25 +0900

    Traverse dir

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -3,10 +3,19 @@ require 'json'
 
 describe "Men's points" do
   it "passing" do
-    File::open("spec/passing/test.json") do |io|
+    count = 0
+    Dir.glob("spec/passing/*.json") do |file|
+      io = open(file)
       data = JSON.load(io)
-      data["name"].should eq "reikubonaga"
-      data["answers"].should eq ["3", "2", "1", "3"]
+      if data["name"] == "reikubonaga"
+        data["answers"].should eq ["3", "2", "1", "3"]
+      elsif data["name"] == "awakia"
+        data["answers"].should eq ["1", "0", "-1", "2"]
+      else
+        raise ("'#{data["name"]}' does not match known names")
+      end
+      count += 1
     end
+    count.should eq 2
   end
 end
